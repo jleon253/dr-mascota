562533d247e7ffbcc947673dd2f61e95bdc08581
jleon253 <jleon253@gmail.com>
2020-05-26 15:09:28 -0500

    Ajustando base deploy y carpeta docs

---
+++ docs/precache-manifest.9764523208859a2ebe7f4eb33c8e9161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f60aa1c1da6268e09985ebf5b6abba4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b0a2e86cecadd6e946b",
+    "url": "./static/css/main.8f1a37f8.chunk.css"
+  },
+  {
+    "revision": "20e9e0d09baf04ae9f50",
+    "url": "./static/js/2.8382c1eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8382c1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b0a2e86cecadd6e946b",
+    "url": "./static/js/main.bc1054ed.chunk.js"
+  },
+  {
+    "revision": "397fec2dc038eb6dace6",
+    "url": "./static/js/runtime-main.929738d8.js"
+  }
+]);
