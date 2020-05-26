1a6f5b94bf8716171198abd624abab7e8eec3b83
jleon253 <jleon253@gmail.com>
2020-05-26 14:40:40 -0500

    Agregando carpeta docs

---
+++ docs/precache-manifest.f6e6a46d37b6e644fac8a8a651f45792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a5f1e2bd3235c3048e1a588c153664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49e1fd1e3860eb8fe494",
+    "url": "/static/css/main.8f1a37f8.chunk.css"
+  },
+  {
+    "revision": "20e9e0d09baf04ae9f50",
+    "url": "/static/js/2.8382c1eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8382c1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e1fd1e3860eb8fe494",
+    "url": "/static/js/main.d73aeb30.chunk.js"
+  },
+  {
+    "revision": "1b13ee7f0369ce8df4c9",
+    "url": "/static/js/runtime-main.4dc01cc0.js"
+  }
+]);
